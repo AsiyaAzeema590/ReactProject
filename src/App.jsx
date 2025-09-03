a17195d6a1948e02e1618f4d1b526687d73b7272
AsiyaAzeema590 <khanasiya3052@gmail.com>
2025-09-03 20:08:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import React from 'react'
+// import './index.css'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='p-4 bg-pink-600 text-3xl '>Chai</h1>
+    </>
+  )
+}
+
+export default App
